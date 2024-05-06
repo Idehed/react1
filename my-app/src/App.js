c841a0c089deb383f5b998759c4d7639a585d59c
Idehed anton <anton.idehed@telia.com>
2024-05-06 17:33:02 +0000

    react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import css from './App.module.css';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+import NavBarSimple from './components/NavBarSimple';
+import StatefulGreetingWithPrevState from './components/SatefulGreetingWithPrevState';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
-    <div className={css.App}>
-      <Sidebar />
+    <div className="App"> 
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
