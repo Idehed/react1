6a7bad665a24108e6828cee0e6fec1c2770927e6
Idehed anton <anton.idehed@telia.com>
2024-05-07 18:27:31 +0000

    forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import css from './App.module.css';
 import Content from './components/Content';
+import ControlledForm from './components/ControlledForm';
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
@@ -9,17 +10,16 @@ import NavBarForm from './components/NavBarForm';
 import NavBarSimple from './components/NavBarSimple';
 import NestingComponents from './components/NestingComponents';
 import StatefulGreetingWithPrevState from './components/SatefulGreetingWithPrevState';
+import SearchBar from './components/SearchBar';
 import Sidebar from './components/Sidebar';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import UnControlledForm from './components/UnControlledForm';
 
 function App() {
   return (
     <div className={css.App}> 
-     <Sidebar/>
-     <NavBarSimple />
-     <NavBarForm />
-     <Content />
+     <SearchBar />
     </div>
   );
 }
